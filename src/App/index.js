e5ad6df04a1b311f801ac32ee73ece880f90619a
carlosfpj <cafer89@gmail.com>
2022-12-03 20:52:07 -0500

    localstorage

---
+++ src/App/index.js
@@ -0,0 +1,72 @@
+import React, { useState } from 'react';
+import { AppUi } from './AppUi.js';
+import './App.css';
+
+// const defaultTodos = [
+//   {text: "cortar cebolla", completed: true},
+//   {text: "Tomar Curso intro React", completed: false},
+//   {text: "Llorar con la Llorona", completed: false},
+//   {text: "Otra asignación", completed: true}
+// ]
+
+function App() {
+
+  const localStorageTodos = localStorage.getItem('TODOS_V1');
+  let parsedTodos;
+
+  if(!localStorageTodos) {
+      localStorage.setItem('TODOS_V1', JSON.stringify([]));
+      parsedTodos = [];
+  } else {
+    parsedTodos = JSON.parse(localStorageTodos);
+  }
+
+  const [todos, setTodos] = useState(parsedTodos);
+  const [searchValue, setSearchValue] = useState('');
+  const completedTodos = todos.filter(todo => !!todo.completed).length;
+  const totalTodos = todos.length;
+
+  let searchedTodos = [];
+
+  if(!searchValue.length >= 1) {
+    searchedTodos = todos;
+  } else {
+    searchedTodos = todos.filter(todo => {
+      const todoText = todo.text.toLowerCase();
+      const searchText = searchValue.toLowerCase();
+      return todoText.includes(searchText);
+    });
+  }
+
+  const saveTodos = (newTodos) => {
+    const stringifiedTodos = JSON.stringify(newTodos);
+    localStorage.setItem('TODOS_V1', stringifiedTodos);
+    setTodos(newTodos);
+  }
+
+  const completeTodo = (text) => {
+    const todoIndex = todos.findIndex(todo => todo.text === text);
+    const newTodos = [...todos];
+    newTodos[todoIndex].completed = !newTodos[todoIndex].completed;
+    saveTodos(newTodos);
+  }
+
+  const deleteTodo = (text) => {
+    const newTodos = todos.filter(todo => todo.text !== text);
+    saveTodos(newTodos);
+  }
+
+  return (
+    <AppUi
+      totalTodos={totalTodos}
+      completedTodos={completedTodos}
+      searchValue={searchValue}
+      setSearchValue={setSearchValue}
+      searchedTodos={searchedTodos}
+      completeTodo={completeTodo}
+      deleteTodo={deleteTodo}
+    />
+  );
+}
+
+export default App;
